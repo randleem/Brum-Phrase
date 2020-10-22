b9197d784a2a40e9543ff6f6e2bb1f737d57a0a7
Isabel Holland <isabel@Isabels-MacBook-Air.local>
2020-10-22 16:42:34 +0100

    Thurs PM

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-// Game object Glass
+// Game object Class
 // States
 // Board
 // Players
@@ -14,6 +14,35 @@
 
 // fileArrayOfQuestion.forEach(setUpBoard);
 
+class Game {
+  constructor(arrayOfQuestions, player1, player2){
+    this.board = new Board (arrayOfQuestions)
+    this.player1 = new Player (player1);
+    this.player2 = new Player (player2);
+  }
+  start(){
+    
+  }
+}
 
 
+
+
+//
+
+  play() {   
+    const move = this.players[this.currentPlayerIndex].gettMove();
+    this.pileSize.takeCoins(move);
+    this.currentPlayerIndex++
+    if(this.currentPlayerIndex > this.players.length){
+      this.currentPlayerIndex = 0;
+    }
+    if (this.pile.getActiveCoins() => 0){
+      console.log(this.players[this.currentPlayerIndex].name, "wins");
+      break
+    }
+    } 
+
+} 
+};
 
