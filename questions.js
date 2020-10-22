ddc92e61f0477a9d3e626401825564c76bc2e363
Isabel Holland <isabel@Isabels-MacBook-Air.local>
2020-10-22 12:01:29 +0100

    Questions done

--- questions.js
+++ questions.js
@@ -3,18 +3,32 @@
     // State
         // Question
         // Answer = true
-        // Picture - // src code for pic
 
     //Behaviour
         // isAnswerCorrect - true false
         // isAnswerTrue - triggers source image to disappear
 
 class Question {
-    constructor(question,  picture){
+    constructor(question, answer){
+        this.number = number
         this.question = question;
-        this.answer = false; // if its true function changes to true
-        this.pictureAnswer = picture; // if the picture is correct answer
+        this.answer = answer;
+        this.answerTile = true; // true when on the table
+    }
+    
+    remove(){
+        this.answerTile = false; 
+        let tile = document.querySelector(`.tile${number}`)
+        tile.addEventListener("click", () => {tile.style = `background-color: " "`})
     }
 
-
 }
+
+
+
+
+
+
+
+
+ 
